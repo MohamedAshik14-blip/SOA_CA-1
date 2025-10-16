3364a2158254bb88fa0c6449a27e019343954b2b
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-10-16 10:17:15 +0100

    Update Program.cs
    Configured WebAssembly host and registered core services
    added HttpClient service for API communication and registered CountryService with dependency injection.

--- Program.cs
+++ Program.cs
@@ -2,31 +2,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TripPlannerApp;
 using TripPlannerApp.Services;
-using Blazored.LocalStorage;
+
 using WeatherApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<CitySuggestionService>();
 
 
-// ✅ Plain HttpClient for external APIs
+
 builder.Services.AddScoped(sp => new HttpClient());
 
-// ✅ Register services
-builder.Services.AddScoped<ILocationService, GeolocationService>();
-builder.Services.AddScoped<ICountryService, CountryService>();
-builder.Services.AddScoped<CitySuggestionService>();
-builder.Services.AddBlazoredLocalStorage();
 
-// ✅ OpenWeather API
-var openWeatherApiKey = "b19e2b255e902a5581acc442253764a0";
-builder.Services.AddScoped<IWeatherService>(sp =>
-{
-    var http = sp.GetRequiredService<HttpClient>();
-    return new OpenWeatherService(http, openWeatherApiKey);
-});
+
+builder.Services.AddScoped<ICountryService, CountryService>();
+
 
 await builder.Build().RunAsync();
