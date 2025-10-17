1ed87fd3cab06ef05b401db0b1b4d88bc877ecda
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-10-17 12:15:13 +0100

    Added ILocationService interface for location management
    Added ILocationService interface for location management
    Declared method to get user's current location and Added method to retrieve location details by city name.

---
+++ Services/ILocationService.cs
@@ -0,0 +1,8 @@
+using WeatherApp.Models;
+
+namespace WeatherApp.Services;
+public interface ILocationService
+{
+    Task<TripLocation> GetUserLocationAsync();
+    Task<TripLocation?> GetLocationByCityAsync(string city);
+}
