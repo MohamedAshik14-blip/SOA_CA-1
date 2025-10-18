4cfb23ce788b03db0709ada8ca9e565a463c41aa
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2025-10-18 20:35:02 +0100

    Configure dependency injection for HttpClient, GeolocationService
    Configure dependency injection for HttpClient, GeolocationService, CountryService, and IWeatherService in Program.cs

--- Program.cs
+++ Program.cs
@@ -14,9 +14,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient());
 
-
-
+builder.Services.AddScoped<ILocationService, GeolocationService>();
 builder.Services.AddScoped<ICountryService, CountryService>();
 
+
+var openWeatherApiKey = "b19e2b255e902a5581acc442253764a0";
+builder.Services.AddScoped<IWeatherService>(sp =>
+{
+    var http = sp.GetRequiredService<HttpClient>();
+    return
+});
 
 await builder.Build().RunAsync();
